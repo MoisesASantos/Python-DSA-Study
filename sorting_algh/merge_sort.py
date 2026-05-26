169f31faa23d89ceb261c0d0126659cc4e2eae24
MoisesASantos <moisessantosa23@gmail.com>
2026-05-26 18:02:35 +0100

    sending the merge_sort

---
+++ sorting_algh/merge_sort.py
@@ -0,0 +1,2 @@
+Our LockedIn influencers are complaining that when they sort their followers by follower count, it gets really slow if they have more than 1,000 followers (because we're using Bubble Sort). Let's speed it up for them with merge sort.
+
