c98d14d20c2fd91de53da69a457619b99b070dfb
MoisesASantos <moisessantosa23@gmail.com>
2026-05-28 13:43:34 +0100

    sending the inorder transversal function on bst

---
+++ Binary_tree/inorder_transversal.py
@@ -0,0 +1,44 @@
+Turns out, the data team had no idea what they were talking about, and our product lead just wanted an export of our tree in sorted order. He wants to be able to see the users in the order they signed up (and were thus given user IDs).
+
+Implement the recursive inorder method. Here are the algorithm's steps:
+
+    Recursively traverse the left subtree
+    Visit the value of the current node by appending its value to the visited array
+    Recursively traverse the right subtree
+    Return the list of nodes visited so far
+
+class BSTNode:
+    def inorder(self, visited):
+        if self.left is not None:
+            self.left.inorder(visited)
+        visited.append(self.val)
+        if self.right is not None:
+            self.right.inorder(visited)
+        return visited
+
+    # don't touch below this line
+
+    def __init__(self, val=None):
+        self.left = None
+        self.right = None
+        self.val = val
+
+    def insert(self, val):
+        if not self.val:
+            self.val = val
+            return
+
+        if self.val == val:
+            return
+
+        if val < self.val:
+            if self.left:
+                self.left.insert(val)
+                return
+            self.left = BSTNode(val)
+            return
+
+        if self.right:
+            self.right.insert(val)
+            return
+        self.right = BSTNode(val)
